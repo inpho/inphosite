9cbe97f3f30b6501c96b909145e9010308eb6e86
Jaimie Murdock <jaimie.murdock@gmail.com>
2011-08-18 16:58:30 -0400

    removed unnecessary static.sh

--- scripts/static.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-paster request ../production.ini /page/index > ../static/index.html
-paster request ../production.ini /page/docs > ../static/docs/index.html
-paster request ../production.ini /page/owl > ../static/owl/index.html
-paster request ../production.ini /page/json > ../static/json/index.html
-paster request ../production.ini /page/papers > ../static/papers/index.html
-paster request ../production.ini /page/faq > ../static/faq/index.html
-paster request ../production.ini /page/graph > ../static/graph/index.html

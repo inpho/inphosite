2eaed062ce6e3ab848c5b6f3f323dedef9a60a35
Jaimie Murdock <jaimie.murdock@gmail.com>
2012-05-14 15:43:58 -0400

    cleaned up bootstrap form elements, fixed inconsistent tab behavior. closes #30

--- inphosite/public/js/admin.js
+++ inphosite/public/js/admin.js
@@ -2,25 +2,50 @@
 inpho = inpho || {};
 inpho.admin = inpho.admin || {};
 
-inpho.admin.process_text = function(e, attr, url) {
-    var status_icon = $('.input-status', $('#'+attr).parent());
+// inpho.admin.process_keydown
+// For use with the onkeydown handler to intercept enter and tab keypresses
+// and trigger form submission.
+inpho.admin.process_keydown = function(e) {
+    var status_icon = $('.input-status', $(this).parent());
     if ((e.keyCode == 13) || (e.keyCode == 9)) { // Enter and Tab support
-        e.preventDefault();
+        // submit the form 
         if (status_icon.hasClass('icon-share-alt'))
-            inpho.admin.submit_field(attr, url);
+            inpho.admin.submit_field(this.id, $(this).attr('data-url'));
+
+        // do not use the browser-submit if enter is pressed
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            return false;
+        }
+    } 
+}
+// bind keydown to .admin-text on document.ready()
+$(function() { $('.admin-text').bind('keydown', inpho.admin.process_keydown) });
+
+// inpho.admin.process_keyup
+// For use with the onkeyup handler to intercept enter and tab keypresses and to
+// change the status icons
+inpho.admin.process_keyup = function(e) {
+    if (e.keyCode == 13) {
+        // do not do the default form submission
+        e.preventDefault();
         return false;
     } else if (e.keyCode == 27) { // Escape 
         return inpho.admin.reset_field(attr, url);
-    } else {
-        var status_icon = $('.input-status', $('#'+attr).parent());
+    } else if (e.keyCode != 9) {
+        // change the status_icon for all keystrokes, other than tab
+        var status_icon = $('.input-status', $(this).parent());
         status_icon.removeClass('icon-ok');
         status_icon.removeClass('icon-warning-sign');
         status_icon.removeClass('icon-loading');
-        $('#'+attr).parents('.control-group').removeClass('success');
-        $('#'+attr).parents('.control-group').removeClass('error');
+        $(this).parents('.control-group').removeClass('success');
+        $(this).parents('.control-group').removeClass('error');
         status_icon.addClass('icon-share-alt');
     }
 }
+// bind keyup to .admin-text on document.ready()
+$(function() { $('.admin-text').bind('keydown', inpho.admin.process_keyup) });
+
 
 inpho.admin.submit_field = function(attr, url) {
   var status_icon = $('.input-status', $('#' + attr).parent());
@@ -71,6 +96,7 @@ inpho.admin.submit_field = function(attr, url) {
   $('#'+attr).parents('.control-group').removeClass('error');
   $('#'+attr).parents('.control-group').removeClass('success');
   
+  status_icon.removeClass('icon-warning-sign');
   status_icon.removeClass('icon-share-alt');
   status_icon.addClass('icon-loading');
   if (attr == "searchpatterns" || attr == "abbrs" || attr == "queries")

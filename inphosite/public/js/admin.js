e980164917fdd47bea38df534f040ef282858c53
Samuel Waggoner <samuel.waggoner@gmail.com>
2012-05-15 12:15:04 -0400

    Changed indentation to 2 spaces rather than 4. closes #41

--- inphosite/public/js/admin.js
+++ inphosite/public/js/admin.js
@@ -18,18 +18,18 @@ inpho.admin = inpho.admin || {};
 // For use with the onkeydown handler to intercept enter and tab keypresses
 // and trigger form submission.
 inpho.admin.process_keydown = function(e) {
-    var status_icon = $('.input-status', $(this).parent());
-    if ((e.keyCode == 13) || (e.keyCode == 9)) { // Enter and Tab support
-        // submit the form 
-        if (status_icon.hasClass('icon-share-alt'))
-            inpho.admin.submit_field(this.id, $(this).attr('data-url'));
+  var status_icon = $('.input-status', $(this).parent());
+  if ((e.keyCode == 13) || (e.keyCode == 9)) { // Enter and Tab support
+    // submit the form
+    if (status_icon.hasClass('icon-share-alt'))
+      inpho.admin.submit_field(this.id, $(this).attr('data-url'));
 
-        // do not use the browser-submit if enter is pressed
-        if (e.keyCode == 13) {
-            e.preventDefault();
-            return false;
-        }
-    } 
+    // do not use the browser-submit if enter is pressed
+    if (e.keyCode == 13) {
+      e.preventDefault();
+      return false;
+    }
+  }
 }
 // bind keydown to .admin-text on document.ready()
 $(function() { $('.admin-text').bind('keydown', inpho.admin.process_keydown) });
@@ -38,35 +38,35 @@ $(function() { $('.admin-text').bind('keydown', inpho.admin.process_keydown) });
 // For use with the onkeyup handler to intercept enter and tab keypresses and to
 // change the status icons
 inpho.admin.process_keyup = function(e) {
-    if (e.keyCode == 13) {
-        // do not do the default form submission
-        e.preventDefault();
-        return false;
-    } else if (e.keyCode == 27) { // Escape 
-        return inpho.admin.reset_field(attr, url);
-    } else if (e.keycode != 9) {
-        $(this).change();  
-    }
+  if (e.keyCode == 13) {
+    // do not do the default form submission
+    e.preventDefault();
+    return false;
+  } else if (e.keyCode == 27) { // Escape
+    return inpho.admin.reset_field(attr, url);
+  } else if (e.keycode != 9) {
+    $(this).change();
+  }
 }
 // bind keyup to .admin-text on document.ready()
 $(function() { $('.admin-text').bind('keyup', inpho.admin.process_keyup) });
 
 inpho.admin.process_change = function(e) {
-    /*alert('during change: ' + $(this).val()
-            + '\ndefault: ' + this.defaultValue);*/
+  /*alert('during change: ' + $(this).val()
+    + '\ndefault: ' + this.defaultValue);*/
 
-    var status_icon = $('.input-status', $(this).parent());
-    if ($(this).val() != this.defaultValue) {
-        status_icon.removeClass('icon-*');
-        $(this).parents('.control-group').removeClass('success');
-        $(this).parents('.control-group').removeClass('error');
-        status_icon.addClass('icon-share-alt');
-    } else {
-        status_icon.removeClass('icon-loading');
-        status_icon.removeClass('icon-warning-sign');
-        status_icon.removeClass('icon-share-alt');
-        status_icon.addClass('icon-ok');
-    }
+  var status_icon = $('.input-status', $(this).parent());
+  if ($(this).val() != this.defaultValue) {
+    status_icon.removeClass('icon-*');
+    $(this).parents('.control-group').removeClass('success');
+    $(this).parents('.control-group').removeClass('error');
+    status_icon.addClass('icon-share-alt');
+  } else {
+    status_icon.removeClass('icon-loading');
+    status_icon.removeClass('icon-warning-sign');
+    status_icon.removeClass('icon-share-alt');
+    status_icon.addClass('icon-ok');
+  }
 }
 $(function() { $('.admin-text').bind('change', inpho.admin.process_change) });
 
@@ -75,41 +75,41 @@ inpho.admin.submit_field = function(attr, url) {
   var status_icon = $('.input-status', $('#' + attr).parent());
   var value = document.getElementById(attr).value;
   if (attr == "URL") {
-      if (value && (value.substring(0, 4) != "http")
-          && (value.substring(0, 4).toLowerCase() != "none"))
-        value = "http://" + value;
-      var value = encodeURIComponent(value);
+    if (value && (value.substring(0, 4) != "http")
+        && (value.substring(0, 4).toLowerCase() != "none"))
+      value = "http://" + value;
+    var value = encodeURIComponent(value);
   }
   else if (attr == "active" || attr == "openAccess" || attr == "student") {
-      if (document.getElementById(attr).checked)
-        value = "1";  
-      else
-        value = "0";
+    if (document.getElementById(attr).checked)
+      value = "1";
+    else
+      value = "0";
   }
-  
+
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
-        if (xhr.status < 400) {
-          status_icon.removeClass('icon-loading');
-          status_icon.addClass('icon-ok');
-          $('#'+attr).parents('.control-group').addClass('success');
-          if (attr == "searchpatterns" || attr == "abbrs" || attr == "queries") {
-              var val = value;
-              val = val.replace("<", "&lt;")
-              val = val.replace(">", "&gt;")
-              var new_entry = '<label><i class="icon-remove" onclick="return inpho.admin.remove(this.parentNode, \'' + attr + '\', \'' + url + '\')"></i>' + val + '</label>';
-              $('#'+attr).before(new_entry);
-              $('#'+attr).val('');
-          } else {
-              // change the default value to the newly submitted value
-              $('#'+attr)[0].defaultValue = value;  
-          }
+      if (xhr.status < 400) {
+        status_icon.removeClass('icon-loading');
+        status_icon.addClass('icon-ok');
+        $('#'+attr).parents('.control-group').addClass('success');
+        if (attr == "searchpatterns" || attr == "abbrs" || attr == "queries") {
+          var val = value;
+          val = val.replace("<", "&lt;")
+          val = val.replace(">", "&gt;")
+          var new_entry = '<label><i class="icon-remove" onclick="return inpho.admin.remove(this.parentNode, \'' + attr + '\', \'' + url + '\')"></i>' + val + '</label>';
+          $('#'+attr).before(new_entry);
+          $('#'+attr).val('');
         } else {
-          status_icon.removeClass('icon-loading');
-          $('#'+attr).parents('.control-group').addClass('error');
-          status_icon.addClass('icon-warning-sign');
+          // change the default value to the newly submitted value
+          $('#'+attr)[0].defaultValue = value;
         }
+      } else {
+        status_icon.removeClass('icon-loading');
+        $('#'+attr).parents('.control-group').addClass('error');
+        status_icon.addClass('icon-warning-sign');
+      }
     }
   }
   xhr.open('PUT', url, true);
@@ -117,7 +117,7 @@ inpho.admin.submit_field = function(attr, url) {
   $('#'+attr+' .input-status').removeClass('icon-*');
   $('#'+attr).parents('.control-group').removeClass('error');
   $('#'+attr).parents('.control-group').removeClass('success');
-  
+
   status_icon.removeClass('icon-warning-sign');
   status_icon.removeClass('icon-share-alt');
   status_icon.addClass('icon-loading');
@@ -127,23 +127,23 @@ inpho.admin.submit_field = function(attr, url) {
     xhr.send(attr + "=" + value);
 }
 
-inpho.admin.remove = function(elt, attr, url) {   
+inpho.admin.remove = function(elt, attr, url) {
   var value = "?pattern=" + encodeURIComponent($(elt).text());
   url = url + value
   var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            $(elt).remove();
-        }
-      }
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      $(elt).remove();
+    }
+  }
   xhr.open('DELETE', url, true);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.send(); 
+  xhr.send();
 }
 
 
 // The reset function does the following:
-// 1.) The text input box should go back to a static field displaying either 
+// 1.) The text input box should go back to a static field displaying either
 //     the new property (on success) or the old property (on failure).
 // 2.) The edit icon should re-appear.
 //
@@ -153,7 +153,7 @@ inpho.admin.reset_field = function(attr, url) {
   // PUT was not successful:
   else
     var attr_value = document.getElementById(attr).value;
-  
+
   if (attr == "searchpatterns" || attr == "abbrs" || attr == "queries")
     $('#'+attr).val('');
 
--- inphosite/public/js/eval.js
+++ inphosite/public/js/eval.js
@@ -2,100 +2,100 @@ var inpho = inpho || {};
 inpho.eval = inpho.eval || {};
 
 inpho.eval.submit = function(elt) {
-    var formElm = elt + '-eval';
-    // antecedent term
-    var id   = $(formElm + " [name='ante_id']").val();
-    // consequent term
-    var id2  = $(formElm + " [name='cons_id']").val();
-    // submitted generality value
-    var generality  = $(formElm + " #generalitySelect").val();
-    // submitted relatedness value
-    var relatedness = $(formElm + " #relatednessSelect").val();
-   
-    // url to post generality value
-    var url_gen = '/idea/' + id + '/generality/' + id2;
-    var url_rel = '/idea/' + id + '/relatedness/' + id2;
-    // submit generality value
-    $.post(url_gen,
-           { degree : generality },
-           function(data){
-               // submit relatedness after generality
-               $.post(url_rel,
-                      { degree : relatedness },
-                      function(data){
-                          // display thanks form
-                          inpho.eval.get_thanks_form(elt);
-                      } 
-               );
-           } );
+  var formElm = elt + '-eval';
+  // antecedent term
+  var id   = $(formElm + " [name='ante_id']").val();
+  // consequent term
+  var id2  = $(formElm + " [name='cons_id']").val();
+  // submitted generality value
+  var generality  = $(formElm + " #generalitySelect").val();
+  // submitted relatedness value
+  var relatedness = $(formElm + " #relatednessSelect").val();
 
-    // url to post relatedness value
+  // url to post generality value
+  var url_gen = '/idea/' + id + '/generality/' + id2;
+  var url_rel = '/idea/' + id + '/relatedness/' + id2;
+  // submit generality value
+  $.post(url_gen,
+         { degree : generality },
+         function(data){
+           // submit relatedness after generality
+           $.post(url_rel,
+                  { degree : relatedness },
+                  function(data){
+                    // display thanks form
+                    inpho.eval.get_thanks_form(elt);
+                  }
+                 );
+         } );
+
+  // url to post relatedness value
 };
 
 inpho.eval.reset = function(elt) {
-    var formElm = elt + '-eval';
-    // antecedent term
-    var id   = $(formElm + " [name='ante_id']").val();
-    // consequent term
-    var id2  = $(formElm + " [name='cons_id']").val();
-    // submitted generality value
-    $(formElm + " #generalitySelect").val('-1');
-    // submitted relatedness value
-    $(formElm + " #relatednessSelect").val('-1');
-    
-    $(elt + " #generalitySelect").attr('disabled', 'disabled');
-    $(elt + " .and").show();
-    $(elt + " #generalitySelect").show();
-    
-    inpho.eval.submit(elt);
+  var formElm = elt + '-eval';
+  // antecedent term
+  var id   = $(formElm + " [name='ante_id']").val();
+  // consequent term
+  var id2  = $(formElm + " [name='cons_id']").val();
+  // submitted generality value
+  $(formElm + " #generalitySelect").val('-1');
+  // submitted relatedness value
+  $(formElm + " #relatednessSelect").val('-1');
+
+  $(elt + " #generalitySelect").attr('disabled', 'disabled');
+  $(elt + " .and").show();
+  $(elt + " #generalitySelect").show();
+
+  inpho.eval.submit(elt);
 };
 
 inpho.eval.get_edit_form = function(elt) {
-    // antecedent term
-    var id   = $(elt + " [name='ante_id']").val();
-    // consequent term
-    var id2  = $(elt + " [name='cons_id']").val();
+  // antecedent term
+  var id   = $(elt + " [name='ante_id']").val();
+  // consequent term
+  var id2  = $(elt + " [name='cons_id']").val();
 
-    var url = "/idea/" + id + "/evaluation/" + id2 + "?edit=1"
-    var form = $.get(url, function(data){ 
-        $(elt + '-eval').alert('close');
-        $(elt).prepend(data);
-    });
+  var url = "/idea/" + id + "/evaluation/" + id2 + "?edit=1"
+  var form = $.get(url, function(data){
+    $(elt + '-eval').alert('close');
+    $(elt).prepend(data);
+  });
 };
 
 inpho.eval.get_thanks_form = function(elt) {
-    // antecedent term
-    var id   = $(elt + " [name='ante_id']").val();
-    // consequent term
-    var id2  = $(elt + " [name='cons_id']").val();
-    // submitted generality value
-    var generality  = $(elt + " #generalitySelect").val();
-    // submitted relatedness value
-    var relatedness = $(elt + " #relatednessSelect").val();
+  // antecedent term
+  var id   = $(elt + " [name='ante_id']").val();
+  // consequent term
+  var id2  = $(elt + " [name='cons_id']").val();
+  // submitted generality value
+  var generality  = $(elt + " #generalitySelect").val();
+  // submitted relatedness value
+  var relatedness = $(elt + " #relatednessSelect").val();
 
-    var url = "/idea/" + id + "/evaluation/" + id2 + "?edit=&relatedness=" + relatedness + "&generality=" + generality;
-    var form = $.get(url, function(data){ 
-        $(elt + '-eval').alert('close');
-        $(elt).prepend(data);
-    });
+  var url = "/idea/" + id + "/evaluation/" + id2 + "?edit=&relatedness=" + relatedness + "&generality=" + generality;
+  var form = $.get(url, function(data){
+    $(elt + '-eval').alert('close');
+    $(elt).prepend(data);
+  });
 };
 
 inpho.eval.validate = function(elt) {
-    if ($(elt + " #relatednessSelect").val() == '0' ||
-        $(elt + " #relatednessSelect").val() == '-1') {
-        $(elt + " #generalitySelect").val('-1');
-        $(elt + " #generalitySelect").attr('disabled', 'disabled');
-    } else {
-        $(elt + " #generalitySelect").attr('disabled', false);
-    }
-    
-    if ($(elt + " #relatednessSelect").val() == '0') {
-        $(elt + " #generalitySelect").val('-1');
-        $(elt + " #generalitySelect").hide();
-        $(elt + " .and").hide();
-    } else {
-        $(elt + " #generalitySelect").show();
-        $(elt + " .and").show();
-        
-    }
+  if ($(elt + " #relatednessSelect").val() == '0' ||
+      $(elt + " #relatednessSelect").val() == '-1') {
+    $(elt + " #generalitySelect").val('-1');
+    $(elt + " #generalitySelect").attr('disabled', 'disabled');
+  } else {
+    $(elt + " #generalitySelect").attr('disabled', false);
+  }
+
+  if ($(elt + " #relatednessSelect").val() == '0') {
+    $(elt + " #generalitySelect").val('-1');
+    $(elt + " #generalitySelect").hide();
+    $(elt + " .and").hide();
+  } else {
+    $(elt + " #generalitySelect").show();
+    $(elt + " .and").show();
+
+  }
 }

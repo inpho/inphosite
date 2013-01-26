364ccf0f447dd70d522cbf6c06540b32f0b948a4
Jaimie Murdock <jaimie.murdock@gmail.com>
2013-01-26 15:25:33 -0500

    pulling out the inpho.eval refactor portions of commit aa6d424171c0250f0d6312b12d1076cd40e84fa0

--- inphosite/public/js/eval.js
+++ inphosite/public/js/eval.js
@@ -2,65 +2,45 @@ var inpho = inpho || {};
 inpho.eval = inpho.eval || {};
 
 
+
+// *********************
+// Evaluation Submission
+// *********************
 inpho.eval.submitEval = function(ante_id, cons_id, rel, gen) {
-  // url to post generality value
-  var url_gen = '/idea/' + ante_id + '/generality/' + cons_id;
-  var url_rel = '/idea/' + ante_id + '/relatedness/' + cons_id;
-  
-  // submit generality value
-  $.post(url_gen,
-         { degree : gen },
-         function(data){
-           // submit relatedness after generality
-           $.post(url_rel,
-                  { degree : rel }
-                );
-         } );
-  // url to post relatedness value
+    console.log("submitEval: " + ante_id + "," + cons_id + "," + rel + "," + gen);
+
+    // url to post generality value
+    var url_gen = '/idea/' + ante_id + '/generality/' + cons_id;
+    var url_rel = '/idea/' + ante_id + '/relatedness/' + cons_id;
+    
+    // submit generality value
+    $.post(url_gen,
+           { degree : gen },
+           function(data){
+             // submit relatedness after generality
+             $.post(url_rel,
+                    { degree : rel }
+                  );
+           } );
+    // url to post relatedness value
 }
 
 inpho.eval.resetEval = function(ante_id, cons_id) {
-  inpho.eval.submitEval(ante_id, cons_id, -1, -1);
+    inpho.eval.submitEval(ante_id, cons_id, -1, -1);
 };
 
+inpho.eval.parseAndSubmit = function(formid) {
+    var anteID = $(document.getElementById(formid)).attr('data-anteID');
+    var consID = $(document.getElementById(formid)).attr('data-consID');
 
+    var relDiv = document.getElementById('relatednessSelect-' + formid);
+    var genDiv = document.getElementById('generalitySelect-' + formid);
 
+    var relVal = inpho.eval.getValueFromButtonGroupDiv(relDiv);
+    var genVal = inpho.eval.getValueFromButtonGroupDiv(genDiv);
 
-// "Pop-up" widget evaluations
-// ***************************
-// TODO refactor parsing form and submittion to use functions above
-inpho.eval.submitWidgetEval = function(elt) {
-  var formElm = elt + '-eval';
-  // antecedent term
-  var id   = $(formElm + " [name='ante_id']").val();
-  // consequent term
-  var id2  = $(formElm + " [name='cons_id']").val();
-
-  // submitted generality value
-  var generality  = $(formElm + " #generalitySelect").val(); 
-  // submitted relatedness value
-  var relatedness = $(formElm + " #relatednessSelect").val(); 
-
-  //console.log("Submit eval = " + id + " " + id2 + " " + generality + " " + relatedness);
-
-  // url to post generality value
-  var url_gen = '/idea/' + id + '/generality/' + id2;
-  var url_rel = '/idea/' + id + '/relatedness/' + id2;
-  // submit generality value
-  $.post(url_gen,
-         { degree : generality },
-         function(data){
-           // submit relatedness after generality
-           $.post(url_rel,
-                  { degree : relatedness },
-                  function(data){
-                    // display thanks form
-                    inpho.eval.get_thanks_form(elt);
-                  }
-                 );
-         } );
-  // url to post relatedness value
-};
+    inpho.eval.submitEval(anteID, consID, relVal, genVal);
+}
 
 inpho.eval.resetWidgetEval = function(elt) {
   var formElm = elt + '-eval';
@@ -80,52 +60,171 @@ inpho.eval.resetWidgetEval = function(elt) {
   inpho.eval.submitWidgetEval(elt);
 };
 
-inpho.eval.get_edit_form = function(elt) {
-  // antecedent term
-  var id   = $(elt + " [name='ante_id']").val();
-  // consequent term
-  var id2  = $(elt + " [name='cons_id']").val();
 
-  var url = "/idea/" + id + "/evaluation/" + id2 + "?edit=1"
-  var form = $.get(url, function(data){
+// ************************************************
+// Full Form (idea-edit.html) Evaluation Submission
+// ************************************************
+inpho.eval.didSelectRelatedness = function(button) {
+    var formid = button.form.id;
+    var generalityDiv = document.getElementById('generalitySelect-' + formid);
+    var submitDiv = document.getElementById('submitDiv-' + formid);
+
+    // update UI
+    inpho.eval.resetButtonGroup(generalityDiv);
+
+    if($(button).val() == 0) { // not related, show submit
+        $(generalityDiv).fadeOut('slow', null);
+        $(submitDiv).fadeIn('fast', null);
+    }
+    else { // are related, show generality
+        $(submitDiv).fadeOut('fast', null);
+        $(generalityDiv).fadeIn('slow', null);
+    }
+}
+
+inpho.eval.didSelectGenerality = function(button) {
+  // update UI, show submit
+  var formid = button.form.id;
+  var submitDiv = document.getElementById('submitDiv-' + formid);
+  $(submitDiv).fadeIn('fast', null);
+}
+
+inpho.eval.didPressSubmit = function(formid) {
+  inpho.eval.parseAndSubmit(formid);
+  inpho.eval.displayThankYou(formid, true);
+}
+
+inpho.eval.displayThankYou = function(formid, shouldUpdateMsg) {
+    // update UI hide form, show thanks/edit
+    $(document.getElementById(formid)).fadeOut('slow', function() {
+        var thanksDiv = document.getElementById("thanksDiv-" + formid);
+        
+        if(shouldUpdateMsg) {
+            var resultSpan = document.getElementById('evalThanksResult-' + formid);
+            resultSpan.innerHTML = inpho.eval.getEvalResultMessage(formid);
+        }
+
+        $(thanksDiv).fadeIn('slow',null);
+    });
+}
+
+inpho.eval.getEvalResultMessage = function(formid) {
+    var anteTerm = $(document.getElementById("thanksDiv-" + formid)).attr('data-ante');
+    var consTerm = $(document.getElementById("thanksDiv-" + formid)).attr('data-cons');
+    var relDiv = document.getElementById('relatednessSelect-' + formid);
+    var genDiv = document.getElementById('generalitySelect-' + formid);
+
+    var relVal = inpho.eval.getHTMLFromButtonGroupDiv(relDiv);
+    var genVal = (inpho.eval.getHTMLFromButtonGroupDiv(genDiv) == null) ? '' : 'and ' + inpho.eval.getHTMLFromButtonGroupDiv(genDiv);
+
+    return '<p class="idea">' + anteTerm + '</p><p> is ' + relVal + ' to ' + genVal + ' </p><p class="idea">' + consTerm + '</p><p>.</p>';
+}
+
+inpho.eval.getValueFromButtonGroupDiv = function(btnGroupDiv) {
+    for(var i = 0; i < $(btnGroupDiv).children().length; i++) {
+        var btn = $($(btnGroupDiv).children()[i]);
+        if(btn.hasClass('active')) {
+            return $(btn).val();
+        }
+    }
+    return -1;
+}   
+
+inpho.eval.getHTMLFromButtonGroupDiv = function(btnGroupDiv) {
+    for(var i = 0; i < $(btnGroupDiv).children().length; i++) {
+        var btn = $($(btnGroupDiv).children()[i]);
+        if(btn.hasClass('active')) {
+            return $(btn).html();
+        }
+    }
+    return null;
+}
+
+inpho.eval.resetButtonGroup = function(btnGroupDiv) {
+    for(var i = 0; i < $(btnGroupDiv).children().length; i++) { 
+        $($(btnGroupDiv).children()[i]).removeClass('active');
+    }
+}
+
+inpho.eval.didPressEdit = function(formid) {
+    inpho.eval.resetButtonGroup(document.getElementById('generalitySelect-' + formid));
+    inpho.eval.resetButtonGroup(document.getElementById('relatednessSelect-' + formid));
+
+    // update UI show form with cancel, hide thanks/edit
+    $(document.getElementById('thanksDiv-' + formid)).fadeOut('slow', function() {
+        $(document.getElementById('cancelBtn-' + formid)).removeClass('hide');
+        $(document.getElementById(formid)).fadeIn('slow', null);
+    });
+}
+
+inpho.eval.didPressCancel = function(formid) {
+    inpho.eval.displayThankYou(formid, false);
+}
+
+
+
+// ****************************************
+// Inline (eval.html) Evaluation Submission
+// ****************************************
+inpho.eval.didPressSubmitInline = function(formid, elt) {
+  inpho.eval.parseAndSubmit(formid);
+  inpho.eval.getThanksForm(formid, elt);
+}
+
+inpho.eval.getThanksForm = function(formid, elt) {
+  var anteID = $(document.getElementById(formid)).attr('data-anteID');
+  var consID = $(document.getElementById(formid)).attr('data-consID');
+
+  var relDiv = document.getElementById('relatednessSelect-' + formid);
+  var genDiv = document.getElementById('generalitySelect-' + formid);
+
+  var relVal = inpho.eval.getValueFromButtonGroupDiv(relDiv);
+  var genVal = inpho.eval.getValueFromButtonGroupDiv(genDiv);
+
+  var url = "/idea/" + anteID + "/evaluation/" + consID + "?edit=&relatedness=" + relVal + "&generality=" + genVal;
+  
+  $.get(url, function(data){
     $(elt + '-eval').alert('close');
     $(elt).prepend(data);
   });
 };
 
-inpho.eval.get_thanks_form = function(elt) {
-  // antecedent term
-  var id   = $(elt + " [name='ante_id']").val();
-  // consequent term
-  var id2  = $(elt + " [name='cons_id']").val();
-  // submitted generality value
-  var generality  = $(elt + " #generalitySelect").val();
-  // submitted relatedness value
-  var relatedness = $(elt + " #relatednessSelect").val();
+inpho.eval.didPressEditInline = function(formid, elt) {
+  inpho.eval.getEvalForm(formid, elt);
+}
 
-  var url = "/idea/" + id + "/evaluation/" + id2 + "?edit=&relatedness=" + relatedness + "&generality=" + generality;
-  var form = $.get(url, function(data){
-    $(elt + '-eval').alert('close');
+inpho.eval.didPressResetInline = function(formid, elt) {
+  var anteID = $(document.getElementById(formid)).attr('data-anteID');
+  var consID = $(document.getElementById(formid)).attr('data-consID');
+
+  inpho.eval.resetEval(anteID, consID);
+  inpho.eval.getEvalFormReset(formid, elt);
+}
+
+inpho.eval.getEvalFormReset = function(formid, elt) {
+  var anteID = $(document.getElementById(formid)).attr('data-anteID');
+  var consID = $(document.getElementById(formid)).attr('data-consID');
+
+  var url = "/idea/" + anteID + "/evaluation/" + consID + "?edit=&relatedness=-1&generality=-1";
+  
+  $.get(url, function(data){
+    $(elt + '-edit').alert('close');
     $(elt).prepend(data);
   });
-};
+}
 
-inpho.eval.validate = function(elt) {
-  if ($(elt + " #relatednessSelect").val() == '0' ||
-      $(elt + " #relatednessSelect").val() == '-1') {
-    $(elt + " #generalitySelect").val('-1');
-    $(elt + " #generalitySelect").attr('disabled', 'disabled');
-  } else {
-    $(elt + " #generalitySelect").attr('disabled', false);
-  }
+inpho.eval.getEvalForm = function(formid, elt) {
+  var anteID = $(document.getElementById(formid)).attr('data-anteID');
+  var consID = $(document.getElementById(formid)).attr('data-consID');
 
-  if ($(elt + " #relatednessSelect").val() == '0') {
-    $(elt + " #generalitySelect").val('-1');
-    $(elt + " #generalitySelect").hide();
-    $(elt + " .and").hide();
-  } else {
-    $(elt + " #generalitySelect").show();
-    $(elt + " .and").show();
+  var url = "/idea/" + anteID + "/evaluation/" + consID + "?edit=1"
+  
+  $.get(url, function(data){
+    $(elt + '-edit').alert('close');
+    $(elt).prepend(data);
+  });
+}
 
-  }
+inpho.eval.didPressCancelInline = function(elt) {
+  $(elt + '-eval').alert('close');
 }

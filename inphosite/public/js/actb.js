c9d9581bf195675a0227c1b60903a1c88254649a
Jaimie Murdock <jaimie.murdock@gmail.com>
2013-06-07 18:17:30 -0400

    migrating to built-in remote data stores added in bootstrap 2.1.0

--- inphosite/public/js/actb.js
+++ inphosite/public/js/actb.js
@@ -19,50 +19,30 @@ inpho.actb.init = function(elt, api_call) {
   }
 
   var url = inpho.util.url(api_call);
-  
-  $(elt).typeahead({sorter: function(items) { return items; }})
-  .on('keyup', function(ev){
-    ev.stopPropagation();
-    ev.preventDefault();
 
-    //filter out up/down, tab, enter, and escape keys
-    if( $.inArray(ev.keyCode,[40,38,9,13,27]) === -1 ){
-      var self = $(this);
-
-      //set typeahead source to empty
-      self.data('typeahead').source = [];
-
-      //active used so we aren't triggering duplicate keyup events
-      if( !self.data('active') && self.val().length > 0){
-
-        self.data('active', true);
-
-        //Do data request. Insert your own API logic here.
-        $.getJSON(url,{
-          q: $(this).val()
-        }, function(data) {
-
-          //set this to true when your callback executes
-          self.data('active',true);
-
-          //Filter out your own parameters. Populate them into an array, since this is what typeahead's source requires
-          var arr = [],
-          i=data.responseData.results.length;
-          while(i--){
-            arr[i] = data.responseData.results[i].label
-          }
-
-          //set your results into the typehead's source
-          self.data('typeahead').source = arr;
-
-          //trigger keyup on the typeahead to make it search
-          self.trigger('keyup');
-
-          //All done, set to false to prepare for the next remote query.
-          self.data('active', false);
+  // solution from pull request:
+  // https://github.com/twitter/bootstrap/pull/3682
+  var labels, mapped;
+  $(elt).typeahead({
+    sorter: function(items) { return items; },
+    source: function(query, process) {
+      $.getJSON(url, {q: query}, function(data) { 
+        labels = [];
+        mapped = {};
+        $.each(data.responseData.results, function(i, item) {
+          mapped[item.label] = item;
+          labels.push(item.label);
         });
-      } // non-duplicate
-    } // non-blank
- }); // initialization
+        
+        process(labels);
+      }); },
+    highlighter: function(item) {
+      return '<a style="color: #333" href="' + mapped[item].url + '">' + item + '</a>';
+    },
+    updater: function(item) {
+      window.location = inpho.util.url(mapped[item].url);
+      return item;
+      }
+  });
 } // initialize_typeahead
 

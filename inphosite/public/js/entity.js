1c6a98e996f36b39d68544221e1216e4058975fd
Alex Frost <alefrost@indiana.edu>
2014-02-11 16:31:19 -0500

    started building mustache version of printList version off of the showMore function

--- inphosite/public/js/entity.js
+++ inphosite/public/js/entity.js
@@ -41,3 +41,36 @@ inpho.entity.showMore = function (attr, id, n, start) {
     }
   });
 }
+
+inpho.entity.showMoreMustache = function (attr, parent_id, n, start) {
+  // build url to grab attribute
+  var url = $('#' + attr).attr('data-source');
+  url += "?limit=" + n + "&start=" + start;
+
+  // get attribute data
+  $.getJSON(url, function (data) {
+    $.get('../templates/printList.mustache', function(template) {
+      // append each item to the list
+      var json = {
+                    "attr": attr,
+                    "parent_id": parent_id,
+                    "n": n,
+                    "start": start,
+                    "results": data.responseData.results
+                 };
+      var html = Mustache.to_html(template, json);// set item as printList.mustache template in the public/template directory
+      $('#' + attr + ' ol .more').before(item);
+      
+
+      // if there are still more entries, correct the "show more" button,
+      // otherwise remove the "show more" button.
+      if (data.responseData.total > (start + n)) {
+        var onClick =  "inpho.entity.showMore('"+attr+"', " + id + ", " + n + ", " + (start + n) + ")";
+        $('#' + attr + ' ol .more a').attr('onClick', onClick);
+        $('#' + attr + ' ol .more a').text('Show more… (' + (data.responseData.total - start - n) + ')');
+      } else {
+        $('#' + attr + ' ol .more').remove();
+      }
+    });
+  });
+}
